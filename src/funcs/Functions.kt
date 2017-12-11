c6d3047011294b4c4da9893c3897a156b7ffd3eb
zhouzechen <188338648782@163.com>
2017-12-11 20:18:46 +0800

    update Lambdas,Functions

--- src/funcs/Functions.kt
+++ src/funcs/Functions.kt
@@ -1,5 +1,196 @@
 package funcs
 
-fun main(args: Array<String>) {
+import com.sun.corba.se.impl.orbutil.graph.Graph
+import sun.security.provider.certpath.Vertex
 
+/**
+ * 方法声明：fun关键字
+ */
+fun double(x: Int): Int = 2 * x
+
+/**
+ * 默认值
+ * 提供默认值的属性在调用时可以忽略
+ * 重写时，不允许存在默认值，必须忽略
+ */
+open class A {
+    open fun foo(i: Int = 10) {}
+}
+
+class B : A() {
+    override fun foo(i: Int) {}  // no default value allowed
+}
+
+fun read(b: Array<Byte>, off: Int = 0, len: Int = b.size) {
+
+}
+
+//如果带有默认值参数位于不带的前面，只有在使用"named arguments"时有效
+fun foo(bar: Int = 0, baz: Int) {
+    println("bar is $bar,baz is $baz")
+}
+
+//但是，如果最后一个参数是lambda表达式，则不允许传入默认值，只能使用默认值
+fun foo(bar: Int = 0, baz: Int = 0, qux: () -> Unit) {
+    println("bar is $bar,baz is $baz")
+}
+
+/**
+ * Named Arguments
+ * 方法参数可以在调用的时候命名，这在方法有大量参数或者参数具有默认值时很方便
+ */
+fun reformat(str: String,
+             normalizeCase: Boolean = true,
+             upperCaseFirstLetter: Boolean = true,
+             divideByCamelHumps: Boolean = false,
+             wordSeparator: Char = ' ') {
+}
+
+/**
+ * 当使用位置参数和命名参数来调用一个函数时，所有的位置参数应该放在第一个名字之前
+ * 如f(1,y = 2)，但是f(x = 1,2)是不允许的
+ */
+/**
+ * 可变参数列表：'vararg'
+ */
+fun foo(vararg strings: String) {
+    strings.map { println(it) }
+}
+//note:命名参数在调用java方法时无法使用，因为java字节码不会保留参数名称
+
+/**
+ * Unit-returning:单位返回
+ * 如果方法不返回任何有用的值，那么返回值类型即是'Unit'。'Unit'类型仅包含一个值--Unit，而且不需要明确的返回（return）
+ * 改中返回类型，返回值类型的声明和返回都是可选的
+ */
+fun printHello(name: String?): Unit {
+    if (name != null)
+        println("hello ${name}")
+    else
+        println("Hi there!")
+    //可选
+    return Unit
+}
+
+/**
+ * 单表达式方法
+ * 当方法只是一个表达式的时候，可以省略大括号
+ */
+//下面方法的返回值类型是可以推断出来的，所以是可以省略的
+fun double(x: Float) = (x * 2).toInt()
+
+/**
+ * 明确的返回值类型
+ * 具有代码体的方法需要明确指出返回类型，除非是返回Unit类型（并且是可选的）。
+ * Kotlin对有代码体的方法不进行返回值推断，因为可能会存在复杂的控制流，对使用者也不友好
+ */
+
+/**
+ * 可变参数：vararg
+ * 参数中只能有一个vararg参数
+ */
+/**
+ * 省略形式调用
+ * 以下情况，可以使用省略号调用：
+ * 1、存在成员方法或者扩展方法时
+ * 2、只有一个参数时
+ * 3、使用'infix'关键字修饰
+ */
+
+//对Int扩展
+infix fun Int.shl(x: Int) = shl(x)
+
+/**
+ * 方法调用范围
+ * 在Kotlin中，可以将方法声明在文件的'top-level'中，并不想java等中需要将方法放在某个类中。
+ * Kotlin的方法也可以声明为局部，成员和扩展方法
+ */
+/**
+ * 局部方法
+ * 方法内部的方法
+ * 局部方法可以使用外部方法的变量，如下面的visited
+ */
+fun dfs(graph: Graph) {
+    val visited = HashSet<Vertex>()
+    fun dfs(current: Vertex) {
+        if (!visited.add(current)) return
+        for (v in current.neighbors)
+            dfs(v)
+    }
+    dfs(graph.vertices[0])
+}
+
+/**
+ * 成员方法
+ * 定义在类或者object内部
+ */
+
+/**
+ * 泛型方法
+ *
+ */
+fun <T> singletonList(item: T): List<T> {
+    return arrayListOf<T>()
+}
+
+/**
+ * inline方法
+ */
+
+
+/**
+ * 扩展方法
+ */
+
+/**
+ * Higher-Order Functions and Lambdas
+ */
+
+/**
+ * Tail recursive：尾递归方法 --- tailrec
+ * 避免循环，不会引发栈溢出
+ * note:tailrec西施的方法，最后一个操作必须调用它自己。在递归调用之后有更多的代码时，不能使用尾递归，并且不能再try/catch中使用。
+ * 目前只有JVM后端支持尾递归
+ */
+//递归计算余弦固定点
+tailrec fun findFixPoint(x: Double = 1.0): Double = if (x == Math.cos(x)) x else findFixPoint(Math.cos(x))
+
+//循环方式为
+private fun findFixPoint(): Double {
+    var x = 1.0
+    while (true) {
+        val y = Math.cos(x)
+        if (x == y) return x
+        x = y
+    }
+}
+
+fun main(args: Array<String>) {
+    //调用具有默认参数的方法
+    read(arrayOf(1, 2), off = 1)
+
+    //调用带有默认参数，后面跟不带的情况
+    foo(baz = 1)
+
+    //baz使用默认值
+    foo(1) { println("Hello") }
+    //bar,baz都是用默认值
+    foo { println("hello") }
+
+    //可以方便的调用
+    reformat("str")
+    //如果没有默认值，调用可能如下,会很麻烦
+    reformat("str", true, true, false, '_')
+    //可以选择几个参数
+    reformat("str", wordSeparator = '_')
+
+    //调用可变参数列表方法
+    foo(strings = *arrayOf("a", "b", "c"))
+
+    //Unit返回类型
+    printHello("Tom")
+
+    //对扩展方法省略方法调用,等同于 1.shl(2)
+    var x = 1 shl 2
+    println(x)
 }
--- src/funcs/Lambdas.kt
+++ src/funcs/Lambdas.kt
@@ -1,5 +1,154 @@
 package funcs
 
-fun main(args: Array<String>) {
+import java.util.concurrent.locks.Lock
 
+/**
+ * Higher-Order Functions
+ * 以方法作为参数或者返回方法的方法称为Higher-Order方法
+ * 例子：lock(),使用一个锁对象和方法，获取锁，执行方法完毕释放锁
+ */
+/**
+ * 'body'是个方法类型：'() -> T'，所有body应该是个无参、返回值类型为T的方法。在try-catch内调用，被lock保护，结果在lock()方法中返回
+ */
+fun <T> lock(lock: Lock, body: () -> T): T {
+    lock.lock()
+    try {
+        return body()
+    } finally {
+        lock.unlock()
+    }
+}
+
+/**
+ * 另一个例子：map()
+ */
+fun <T, R> List<T>.map(transform: (T) -> R): List<R> {
+    val result = arrayListOf<R>()
+    for (item in this)
+        result.add(transform(item))
+    return result
+}
+
+/**
+ * it : 单个参数时的隐式对象
+ */
+
+/**
+ * 下划线：(1.1之后)
+ * 如果lambda表达式中有不使用的参数，可以使用下划线代替变量名
+ */
+
+/**
+ * lambda简介
+ * 1、lambda表达式总是被大括号包裹
+ * 2、参数在'->'之前定义（参数类型可以省略）
+ * 3、实体跟在'->'之后
+ */
+/**
+ * lambda表达式和匿名方法
+ * lambda表达式或者是匿名方法被称为"function literal"（一个没被声明，而是通过表达式立即使用的形式）
+ */
+//max(strings, { a, b -> a.length < b.length })
+//相当于
+fun compare(a: String, b: String): Boolean = a.length < b.length
+
+/**
+ * 方法类型
+ * 为了方法能够接收方法作为参数，必须使用特别的方法类型作为参数
+ * 例如：
+ */
+fun <T> max(collection: Collection<T>, less: (T, T) -> Boolean): T? {
+    var max: T? = null
+    for (it in collection)
+        if (max == null || less(max, it))
+            max = it
+    return max
+}
+
+/**
+ * 参数less是个(T,T) -> Boolean的类型 -------- 两个参数都是T，并且返回值为Boolean的方法
+ */
+
+/**
+ * 完整Lambda表达式
+ * val sum = {x: Int,y: Int -> x + y}
+ * 被大括号包裹，参数声明和可选的类型注解，代码体在 '->' 后面
+ * 如果表达式的返回值不是Unit，代码体的最后一个表达式被当做返回值（可以使用return明确指出返回值--@）
+ * 如果保留所有可选项，应该：
+ * val sun:(Int,Int) -> Int = {x,y -> x + y}
+ * 如果表达式只有一个参数，可以使用it代替
+ */
+//note:如果方法参数中的最后一个参数是方法，lambda表达式可以在外面的括号中传递
+
+
+/**
+ * 匿名方法
+ * lambda表达式中唯一缺少的是返回值类型，编译器自动推测类型，如果需要明确指出返回类型，可以使用匿名方法（不能是类成员）
+ * 方法的参数和返回类型和普通的方法使用一样；如果参数的类型可以通过上下文推断出来，可以省略
+ */
+//note:匿名方法参数多数在括号内，只有在lambda表达式中可以将参数写在括号外
+/**
+ * lambda表达式和匿名方法的不同还在于'non-local returns'
+ * 没有标签的返回语句总是从用fun关键字声明的函数返回。 这意味着lambda表达式中的返回将从封闭函数返回，而匿名函数中的返回将从匿名函数本身返回
+ */
+
+/**
+ * 闭包
+ * 一个lambda表达式或者匿名方法可达自己的内部，不像Java，Kotlin可以修改闭包中获取到的外部的值
+ */
+
+
+/**
+ * Function Literals with Receiver:方法文字和接收器
+ * Kotlin提供使用指定接收器调用方法文字的能力。在方法文字体内，可以不额外增加定义调用接收器对象的方法
+ * 类似于扩展方法，
+ */
+//todo 未完待续 https://kotlinlang.org/docs/reference/lambdas.html
+//sum:Int.(other:Int) -> Int
+//使用时：1.sum(2)
+
+
+fun main(args: Array<String>) {
+    fun toBeSync() = arrayListOf<Int>(1, 2, 3)
+    val result = lock(lock, ::toBeSync)
+    //通过lambda表达式
+    val res = lock(lock, { arrayListOf<Int>(1, 2) })
+
+    /**
+     * Kotlin中，如果方法的最后一个参数是个方法类型，可以通过lambda表达式最为参数，可以用括号写在外面
+     */
+    lock(lock) {
+        arrayListOf<Int>(1)
+    }
+
+    //调用map方法
+    val doubled = arrayListOf<Int>().map { value -> value * 2 }
+
+    //使用it
+    arrayListOf<Int>().map { it * 2 }
+
+    //LINQ风格
+    var strings = arrayListOf<String>("dfsgd", "dfgae", "eegdc", "dd", "dsss")
+    strings.filter { it.length == 5 }.sortedBy { it }.map { it.toUpperCase() }
+
+    //下划线
+    mapOf<String, String>().forEach { _, value -> println() }
+
+    //明确指出返回值
+    arrayOf(1, 2, 3).filter {
+        val shouldFilter = it > 0
+        return@filter shouldFilter
+    }
+
+    //匿名方法不能是类成员
+    fun(x: Int, y: Int): Int = x + y
+    //省略匿名方法的参数类型
+    arrayOf(1, 2, 3).filter(fun(item) = item > 0)
+
+    //闭包
+    var sum = 0
+    arrayOf(1, 3, 4, 5, -1, -2).filter { it > 0 }.forEach {
+        sum += it
+    }
+    println("sum is $sum")
 }
